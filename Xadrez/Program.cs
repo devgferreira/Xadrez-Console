635909d3f3fa30e9447f5dc346604a4c44ce4f35
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-03-16 13:28:42 -0300

    feat: Melhorando a implementação do método imprimirTabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -5,9 +5,9 @@ using xadrez;
 try
 {
     Tabuleiro tab = new Tabuleiro(8, 8);
-    tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-    tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(1, 9));
-    tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(2, 4));
+    tab.colocarPeca(new Torre(Cor.Branca, tab), new Posicao(0, 0));
+    tab.colocarPeca(new Rei(Cor.Branca, tab), new Posicao(1, 3));
+    tab.colocarPeca(new Torre(Cor.Amarela, tab), new Posicao(2, 4));
 
 
     Tela.imprimirTabuleiro(tab);
--- Xadrez/Tabuleiro/Cor.cs
+++ Xadrez/Tabuleiro/Cor.cs
@@ -9,9 +9,6 @@ namespace tabuleiro
    enum Cor
     {   
         Branca, 
-        Preta,
         Amarela,
-        Azul, 
-        Vermelha
     }
 }
