7ef8bf4bfe33d698daa080aec0e547ebe4b45606
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2023-03-16 15:00:14 -0300

    feat: Implementando o começo da logica de moviemento possiveis das peças

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -15,6 +15,11 @@ try
         Console.WriteLine();
         Console.Write("Origem: ");
         Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+        // limpar o tab para imprimir com as possiveis posições
+        bool[,] posicoesPossiveis = partida.tab.peca(origem).movPossiveis();
+        Console.Clear();
+        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+        Console.WriteLine();
         Console.Write("Destino: ");
         Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
